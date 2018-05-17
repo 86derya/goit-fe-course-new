36a621f14071dfe5ef38bfc01cd27f958a3dc8fe
86derya <derya_86@ukr.net>
2018-05-17 06:44:50 +0400

    project320 init

---
+++ html-css/project/smartgrid.js
@@ -0,0 +1,45 @@
+var smartgrid = require('smart-grid');
+
+/* It's principal settings in smart grid project */
+var settings = {
+    filename: "_smartgrid",
+    outputStyle: 'scss',
+    /* less || scss || sass || styl */
+    columns: 12,
+    /* number of grid columns */
+    offset: '0px',
+    /* gutter width px || % */
+    mobileFirst: 'mobileFirst',
+    /* mobileFirst ? 'min-width' : 'max-width' */
+    container: {
+        maxWidth: '1150px',
+        /* max-width оn very large screen */
+        fields: '0px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            width: '1920px',
+            /* -> @media (max-width: 1100px) */
+        },
+        sm: {
+            width: '768px',
+            offset: '30px',
+            fields: '0px' /* set fields only if you want to change container.fields */
+        },
+        xs: {
+            width: '320px',
+            fields: '0px'
+        }
+        /* 
+        We can create any quantity of break points.
+ 
+        some_name: {
+            width: 'Npx',
+            fields: 'N(px|%|rem)',
+            offset: 'N(px|%|rem)'
+        }
+        */
+    }
+};
+
+smartgrid('./src/precss', settings);
