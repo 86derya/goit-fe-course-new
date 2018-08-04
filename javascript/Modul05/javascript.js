b3ea7f77e02e810b2c16217382d0854c5a972f53
86derya <derya_86@ukr.net>
2018-08-04 19:35:44 +0400

    hw06 init

--- javascript/Modul05/javascript.js
+++ javascript/Modul05/javascript.js
@@ -173,12 +173,8 @@ console.log(getUsersByFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree
 
 const getAllSkills = arr => arr
     .reduce((skillsCollection, user) => user.skills ? skillsCollection.concat(user.skills) : skillsCollection, [])
-
-
-
-
-
-
+    .filter((skill, index, skills) => skills.indexOf(skill) === index)
+    .sort()
 console.log(getAllSkills(users));
 // // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
 
@@ -186,7 +182,9 @@ console.log(getAllSkills(users));
 // /**
 // * Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 // */
-// const getUserNamesSortedByFriendsCount = arr => {...};
+const getUserNamesSortedByFriendsCount = arr => arr
+    .sort((a, b) => a.friends.length > b.friends.length)
+    .map(user => user.name)
 
-// console.log(getUserNamesSortedByFriendsCount(users)); 
+console.log(getUserNamesSortedByFriendsCount(users));
 // // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
