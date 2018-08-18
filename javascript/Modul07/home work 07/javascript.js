8feb3a9bc4f04be67a55b7db516956ae408da435
86derya <derya_86@ukr.net>
2018-08-18 15:09:43 +0300

    module 07 homework init

--- javascript/Modul07/home work 07/javascript.js
+++ javascript/Modul07/home work 07/javascript.js
@@ -1,23 +1,78 @@
-/*
-  Есть список категорий с классом categories (на вкладке HTML).
-  
-  Напишите код, который для каждого элемента li (первая вложенность) 
-  в списке categories выведет в консоль:
-  - Текст непосредственно в нём (название категории)
-  - Количество всех вложенных в него элементов li
-  
-  К примеру:
-    Категория: Животные
-    Количество вложенных li: 4
-*/
-const list = document.querySelectorAll(".categories > li");
-console.log("list", list)
-const Arr = Array.from(list);
-console.log("Arr", Arr)
-for (i of list) {
-    let categoryName = i.firstChild;
-    const contentArr = Array.from(i.querySelectorAll("li"));
-    const Arr = contentArr.map(elem => elem = elem.textContent)
-    console.log("Категория:", categoryName, "Количество вложенных li:", i.querySelectorAll("li").length, " контент категории:", Arr)
+// 1. Модифицируйте готовую функцию createPostCard() из задания 
+//     номер 6 (https://codepen.io/goit-fe-adv/pen/MVPaeZ) так, 
+//     чтобы она принимала объект post с данными для заполнения полей 
+//     в карточке.
 
+//   2. Создайте функцию createCards(posts), которая принимает массив
+//     объектов-карточек, вызывает функцию createPostCard(post) столько
+//     раз, сколько объектов в массиве, сохраняя общий результат и возвращает 
+//     массив DOM-элементов всех постов.
+
+//   3. Повесьте все посты в какой-то уже существующий DOM-узел.
+// */
+
+
+const posts = [{
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 1",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-1.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/nature",
+        title: "Post title 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-2.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-3.com'
+    }
+];
+
+
+
+const body = document.querySelector("body");
+const postCardsContainer = document.createElement("div");
+body.append(postCardsContainer);
+// __________________________________________________________________________________________
+
+
+
+const createPostCard = ({ img, title, text, link }) => {
+    const postCard = document.createElement("div");
+    postCard.classList.add("post");
+
+    const postImage = document.createElement("img");
+    postImage.classList.add('post__image');
+    postImage.setAttribute('src', img);
+    postImage.setAttribute('alt', 'post image');
+
+    const postTitle = document.createElement("h2");
+    postTitle.classList.add("post__title");
+    postTitle.innerText = title;
+
+    const postText = document.createElement("p");
+    postText.classList.add("post__text");
+    postText.innerText = text;
+
+    const postButton = document.createElement("a");
+    postButton.classList.add("button");
+    postButton.innerText = link;
+    postButton.setAttribute('href', '#')
+
+    postCard.append(postImage, postTitle, postText, postButton);
+    return postCard;
 }
+
+
+const createPostCards = Arr => {
+    const postCards = Arr.map(post => createPostCard(post));
+
+    postCardsContainer.append(...postCards)
+    return postCards
+};
+
+createPostCards(posts);
