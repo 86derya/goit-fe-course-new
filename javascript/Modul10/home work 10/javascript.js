0862d18bad9902a65ecf30059024f3e3d371773f
86derya <derya_86@ukr.net>
2018-09-15 11:24:24 +0400

    erger

--- javascript/Modul10/home work 10/javascript.js
+++ javascript/Modul10/home work 10/javascript.js
@@ -23,21 +23,33 @@
 const DOMrefs = {
     allUsers: document.querySelector(".all_users__container"),
     getAllUsersBtn: document.querySelector(".submitGetAllUsersBtn"),
-}
+    allFetchedUsersWrapper: document.querySelector(".user-list-wrapper"),
 
-const user = {
-    age: null,
-    id: null,
-    name: '',
+    findUserByIdContainer: document.querySelector(".user_getByid_container"),
+    findUserByIdForm: document.querySelector(".user_getByid_form"),
+    findUserByIdInput: document.querySelector(".user_getByid_id"),
+    findUserByIdWrapper: document.querySelector(".user_gotById-wrapper"),
+    findUserByIdName: document.querySelector(".user_gotById-name"),
+    findUserByIdUserId: document.querySelector(".user_gotById-id"),
+    findUserByIdAge: document.querySelector(".user_gotById-age"),
+
+    createNewUserForm: document.querySelector(".New-user_create__form"),
+    createNewUserBtn: document.querySelector(".New-user_create__button"),
+    createNewUserName: document.querySelector(".New-user_create__name"),
+    createNewUserAge: document.querySelector(".New-user_create__age"),
 }
 
 const request = {
     API_URL: "https://test-users-api.herokuapp.com/users/",
+    UserID: null,
+    UserName: null,
+    UserAge: null,
 }
 
+// ==============getAllUsers()================================================================================
 function createUserList(users) {
     return users.reduce((acc, user) => acc +
-        `<div class="user-wrapper"> 
+        `<div class="user-card"> 
         <p class="user-name"> user Name: ${user.name}</p>
         <p class="user-id">user ID: ${user.id}</p> 
         <p class="user-age"> user Age: ${user.age} </p> 
@@ -45,36 +57,159 @@ function createUserList(users) {
     )
 }
 
+function removeUserList() {
+    DOMrefs.allFetchedUsersWrapper.innerHTML = "";
+}
+
+function updateAllUsersBtnActive() {
+    DOMrefs.getAllUsersBtn.textContent = "Update List";
+}
+
 function getAllUsers() {
 
     fetchAllUsers().then(users => {
         const markup = createUserList(users);
-        DOMrefs.allUsers.insertAdjacentHTML('beforeend', markup);
+        DOMrefs.allFetchedUsersWrapper.insertAdjacentHTML('beforeend', markup);
+
     })
 
 }
 
 function fetchAllUsers() {
-    fetch(request.API_URL, {
-            method: 'GET',
-            body: JSON.stringify(),
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            }
-        }).then(response => {
-            if (response.ok) return response.json();
-            throw new Error(`Error while fetching: ${response.statusText}`)
-        }).then(data => {
-            console.log(data);
-            return data.data;
-        })
-        .catch(error => {
-            console.log("ERROR:" + error)
-        })
+    return fetch(request.API_URL, {
+        method: 'GET',
+        body: JSON.stringify(),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }).then(response => {
+        if (response.ok) return response.json();
+        throw new Error(`Error while fetching: ${response.statusText}`)
+    }).then(data => {
+        console.log(data);
+        return data.data;
+    }).catch(error => {
+        console.log("ERROR:" + error)
+    })
+
+}
+
+function handleGetAllUsers() {
+    updateAllUsersBtnActive();
+    removeUserList();
+    getAllUsers();
+
+
+}
+
+// ======================getUserById(id)==============================================================
+function updateUserFoundById(user) {
+
+    DOMrefs.findUserByIdName.textContent = `User name:   ${user.name}`;
+    DOMrefs.findUserByIdUserId.textContent = `User ID: ${user.id}`;
+    DOMrefs.findUserByIdAge.textContent = `User Age:  ${user.age}`;
+}
+
+function resetUserFoundById() {
+
+    DOMrefs.findUserByIdName.textContent = `User name:`;
+    DOMrefs.findUserByIdUserId.textContent = `User ID:`;
+    DOMrefs.findUserByIdAge.textContent = `User Age:`;
+}
+
+function fetchUserById(id) {
+    return fetchAllUsers().then(
+        users => {
+            return users.find(user => user.id === id);
+        }
+    ).then(founduser => {
+        if (founduser) return founduser;
+        throw new Error(`Error while fetching: ${response.statusText}`)
+    })
+}
+
+function getUserById(id) {
+    return fetchUserById(id).then(
+        user => {
+            console.log(`User fetched By Id: 
+            User Name "${user.name}",
+            User ID "${user.id}",
+            User Age "${user.age}".`);
+            updateUserFoundById(user);
+        },
+        error => {
+            console.log(" ERROR:" + error);
+            alert(`User ID   "${request.UserID}"  is Invalid `);
+        }
+    )
+}
+
+function handleGetUserById(evt) {
+    evt.preventDefault();
+    const target = evt.target;
+    if (target.nodeName !== "BUTTON") return;
+    if (!target.textContent === "Find user") return;
+    resetUserFoundById();
+    request.UserID = DOMrefs.findUserByIdInput.value;
+
+    getUserById(request.UserID)
+    DOMrefs.findUserByIdForm.reset();
+}
+
+
+// ==========================addUser(name, age)==========================================================
+
+function addUser(nameToAdd, ageToAdd) {
+
+    const user = {
+        name: nameToAdd,
+        age: ageToAdd,
+    }
+
+    return fetch(request.API_URL, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }).then(response => {
+        return response.json();
+    }).then(
+        newuser => {
+            return console.log(newuser);
+        }
+
+    ).catch(error => {
+        alert(`User ID   "${error}"  is Invalid `);
+        console.log("ERROR:" + error)
+    })
+}
+
+function handleCreateNewUser(e) {
+    e.preventDefault();
+
+    request.UserName = DOMrefs.createNewUserName.value;
+    request.UserAge = DOMrefs.createNewUserAge.value;
+
+    if (request.UserAge === "" || request.UserName === "") {
+        DOMrefs.createNewUserForm.reset();
+        return alert("all fields should be filled properly");
+    }
+
+    addUser(request.UserName, request.UserAge);
+    DOMrefs.createNewUserForm.reset();
+}
+
+// ==================removeUser(id)==========================================================
+
+function removeUser(id) {
 
 }
 
 
 
-getAllUsers()
+DOMrefs.getAllUsersBtn.addEventListener('click', handleGetAllUsers);
+DOMrefs.findUserByIdForm.addEventListener('click', handleGetUserById);
+DOMrefs.createNewUserBtn.addEventListener('click', handleCreateNewUser);
