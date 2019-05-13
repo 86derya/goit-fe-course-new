6592f167715534abce82f61e1669693ae7db9613
Andrii Dederia <derya_86@ukr.net>
2019-05-13 16:28:03 +0300

    pixabay API_KEY changed

--- javascript/FindPic/src/js/services/api.js
+++ javascript/FindPic/src/js/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_KEY = '10456559-0f017c1b081538875f387541b';
+const API_KEY = '10456559-ece128cde22be6e54d79b9b25';
 
 export const getImages = ({ query, page }) => {
   const url = `https://pixabay.com/api/?image_type=photo&q=${query}&per_page=12&page=${page}&key=${API_KEY}`;
